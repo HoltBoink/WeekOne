69ab90fe330f155f51b261c48f2144b0600ce352
HoltBoink <boinkh@bc.edu>
2023-01-23 17:51:39 -0500

    Initial Commit

---
+++ WeekOne/WeekOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeekOneApp.swift
+//  WeekOne
+//
+//  Created by Holt Boink on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct WeekOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
